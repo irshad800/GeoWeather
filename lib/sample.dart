a2dbe72bcd7c1f1eee1707b76ff2f710a5e555b8
MUHAMMED IRSHAD VP <irshadvp800@gmail.com>
2024-08-08 08:46:43 +0530

    created a project Geoweather and installed firebased to the project

---
+++ lib/sample.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class sample extends StatelessWidget {
+  const sample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
